c9fcf6355a349b7ae11c2c19c3bbf1f397212385
Nicholas Tanugroho <nicholas.tanugroho@ti.ukdw.ac.id>
2023-12-12 23:59:53 +0700

    Update SJF Preemtive.py

--- SJF Preemtive.py
+++ SJF Preemtive.py
@@ -17,6 +17,7 @@ class SFJPreemtive:
         minm = 999999999
         short = 0
         check = False
+        complete = 0
 
         while (complete != n):
             for j in range(n):
@@ -68,6 +69,28 @@ class SFJPreemtive:
         ATAT = total_tat / n
         return akhir, AWT, ATAT
 
+def center_window(window, width, height):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+
+    window.geometry(f"{width}x{height}+{x}+{y}")
+
+class CustomInputDialog(simpledialog.Dialog):
+    def __init__(self, parent, title, prompt):
+        self.prompt = prompt
+        super().__init__(parent, title=title)
+
+    def body(self, parent):
+        tk.Label(parent, text=self.prompt).grid(row=0, column=0)
+        self.entry = tk.Entry(parent, width=30)
+        self.entry.grid(row=0, column=1)
+        return self.entry 
+
+    def apply(self):
+        self.result = self.entry.get()
 
 class ProgramSJFPreemtive:
 
@@ -78,31 +101,35 @@ class ProgramSJFPreemtive:
         self.create_widgets()
 
     def create_widgets(self):
-        self.jumlahproses = tk.IntVar(value=5)
-        ttk.Label(self.root, text="Silahkan Masukan Jumlah Proses:").grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
-        ttk.Spinbox(self.root, from_=1, to=10, textvariable=self.jumlahproses).grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
+        self.jumlahproses = tk.IntVar(value=3)
+        ttk.Label(self.root, text="Silahkan Masukan Jumlah Proses:").grid(row=1, column=0, padx=5, pady=10, sticky=tk.W)
+        ttk.Spinbox(self.root, from_=1, to=10, textvariable=self.jumlahproses).grid(row=1, column=1, padx=5, pady=15, sticky=tk.W)
 
-        ttk.Button(self.root, text="MULAI", command=self.run_simulation).grid(row=1, column=0, columnspan=2, pady=10)
-
-        self.result_text = tk.Text(self.root, height=50, width=80)
-        self.result_text.grid(row=2, column=0, columnspan=2, padx=10, pady=5)
+        ttk.Button(self.root, text="MULAI", command=self.run_simulation).grid(row=1, column=1, columnspan=2, padx=200, pady=10)
+        
+        self.result_text = tk.Text(self.root, height=30, width=60)
+        font = self.result_text.cget("font")
+        font_family, _, _ = font.partition(" ")
+        new_font = (font_family, 12)
+        self.result_text.configure(font=new_font)
+        self.result_text.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
 
     def run_simulation(self):
         self.processes = []
         num_processes = self.jumlahproses.get()
-
+        result_str = "Arrival Time => Waktu kedatangan proses\nBurst Time => Waktu yang dibutuhkan untuk menyelesaikan proses\n\n"
         for i in range(num_processes):
-            arrival_time = int(simpledialog.askstring(f"Proses {i + 1}", "Masukan Arrival Time: "))
-            burst_time = int(simpledialog.askstring(f"Proses {i + 1}", "Masukan Burst Time: "))
+            arrival_time = int(CustomInputDialog(root,title=(f"Arrival Time Proses {i + 1}"),prompt="Masukan Arrival Time :").result)
+            burst_time = int(CustomInputDialog(root,title=(f"Burst Time Proses {i + 1}"),prompt="Masukan Burst Time :").result)
             self.processes.append([i + 1, arrival_time, burst_time]) 
 
         sfj = SFJPreemtive(self.processes)
         akhir, AWT, ATA = sfj.findavgTime(self.processes, num_processes)
 
-        result_str = "\n"
-        result_str += "Processes\tBurst Time\tWaiting Time\tTurn-Around Time\n"
+        result_str += "\n\nHASIL PERHITUNGAN\n\n"
+        result_str += "Processes\tBurst Time\tWaiting Time\tTurn-Around Time\n\n"
         for i in akhir:
-            result_str += f"\tP{i[0]}\t{i[1]}\t{i[2]}\t{i[3]}\n"
+            result_str += f"    P{i[0]}\t\t{i[1]}\t  {i[2]}\t\t  {i[3]}\n"
         result_str += "\n"
         result_str += f"\nAverage Waiting Time: {AWT:.2f}"
         result_str += f"\nAverage Turn Around Time: {ATA:.2f}"
@@ -114,5 +141,6 @@ class ProgramSJFPreemtive:
 
 
 root = tk.Tk()
+center_window(root, 565, 650)
 app = ProgramSJFPreemtive(root)
 root.mainloop() 
